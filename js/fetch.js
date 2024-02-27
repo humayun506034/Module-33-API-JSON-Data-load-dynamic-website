7862ea9f91917b7fd312ef50e48e39039065b8e0
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-02-27 23:33:02 +0600

    Module 33 API JSON Data load dynamic website

---
+++ js/fetch.js
@@ -0,0 +1,14 @@
+/*
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json()) // .json is not similer but close to JSON.parse()
+    .then(json => console.log(json))    
+
+ */
+
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json()) // .json is not similer but close to JSON.parse()
+        .then(json => console.log(json))
+}
